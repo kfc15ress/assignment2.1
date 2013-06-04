f78c5b0a928277d4744ad88e75a73172cf60a7a3
Crystal <kfc15ress@gmail.com>
2013-06-03 23:55:19 -0400

    Lectures done too
    Sorry this took forever. :/

---
+++ lectures.php
@@ -0,0 +1,95 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <meta charset="utf-8">
+ <meta name="author" content="Robert M. Erickson">
+ <title>CS 142 Advanced Web Design: CSS and Usability</title>
+ <meta name="description" content="This course provides an in depth exploration of the foundation of  Cascading Style Sheets and covering Web Usability to help you create a pleasing user experience for visitors to your site."/>
+<!-- https://www.uvm.edu/~rerickso/education/classes/2013_Summer/css/base.css -->
+<!-- css/base.css -->
+<link href="clbaldwi.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="print.css" rel="stylesheet" type="text/css" media="print" />
+<link rel="shortcut icon" href="https://www.uvm.edu/~rerickso/education/classes/2013_Summer/favicon.ico" >
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+<script type="text/javascript">
+<!--
+// keeps us from getting framed
+		if (top.location != self.location) {
+			top.location.replace(self.location)
+		}
+window.onresize = function() { document.getElementById("screenSize").innerHTML = " " + window.innerWidth + " x " + window.innerHeight; }
+//-->
+</script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32182956-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+</script>
+</head>
+
+<body id="lectures">
+<div id="wrapper"> <!-- Main wrapper for CSS --> 
+<div id="candyWrapper"> <!-- all the header info wrapper for CSS --> 
+<header> <!-- ************ header ************ -->
+		<h1 id="classtitle">
+<a href="http://www.uvm.edu/~cems/cs/?Page=default.php">CS</a> 142 
+<a href="syllabus.php?classRegNum=60649" class="url">Advanced Web Design: CSS and Usability</a> - 2013
+</h1>
+<nav id="toolmenu"><span id="screenSize"></span><ol>
+<li><a href="https://bb.uvm.edu/webapps/portal/frameset.jsp" target="_blank">Blackboard</a></li>
+<li><a href="http://www.uvm.edu/~rerickso/education/classesITeach.php">Classes</a></li>
+<li><a href="https://myuvm.uvm.edu/cp/home/displaylogin" target="_blank">MyUVM</a></li>
+</ol></nav>
+</header> <!-- ************ end header  ************ -->
+
+<div id="studentInfo"> <!-- %^%^%^%^%^ studentInfo %^%^%^%^%^ -->
+<span id="studentName">
+Student: clbaldwi
+</span>
+<span id="studentGrade" >Current <a href="grades.php">Grade</a>: 88</span>
+<span id="classRank">
+Rank: 12 of 8
+</span>
+</div> <!-- %^%^%^%^%^ student info %^%^%^%^%^ -->
+
+<nav id="mainMenu"> <!-- %^%^%^%^%^%^%^%^%^%^%^%^ Navigation %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^% -->
+<ol>
+<li class="menuLink"><a href="assignments.php">Assignments</a></li>
+<li class="menuLink"><a href="calendar.php?abWeek=10">Calendar</a></li>
+<li class="menuLink activeLink"><a href="lectures.php">Lectures</a></li>
+<li class="menuLink"><a href="sick.php">Sick</a></li>
+<li class="menuLink"><a href="syllabus.php">Syllabus</a></li>
+<li class="menuLink"><a href="submitAssignment.php">Submit Assignment</a></li>
+</ol>
+</nav> <!-- navigation -->
+<!-- ######################## adminMenu ######################## -->
+<nav id='adminMenu' class='menu gradesPage'>
+<ol>
+		<li class="menuAdmin menuLink"><a href="sample.php">Attendance</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Class List</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Grading Criteria</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Enter Grades</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Class Report</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Group Report</a></li>
+		<li class="menuAdmin menuLink"><a href="sample.php">Summary</a></li>
+	</ol>
+</nav>
+<!-- ######################## adminMenu ######################## -->
+</div> <!-- ends candyWrapper -->
+<section id="mainContent"> <!-- ######################## main Content ######################## --><h2>Lectures</h2><ol class="LecturesLink"><li class="title summary">2013-05-20 - <a class="url" href="lecture.php?lid=153">CSS: Ch 1, 2: CSS and Documents, Selectors</a></li><li class="title summary">2013-05-21 - <a class="url" href="lecture.php?lid=154">CSS: Ch 3, 4: Structure and the Cascade, Units</a></li><li class="title summary">2013-05-22 - <a class="url" href="lecture.php?lid=155">CSS: Ch 5, 6, 7: Fonts, Text, Basic Visual Formatting</a></li><li class="title summary">2013-05-28 - <a class="url" href="lecture.php?lid=156">CSS: Ch 8, 9, 10</a></li><li class="title summary">2013-05-29 - <a class="url" href="lecture.php?lid=157">CSS: Ch 11, 12, 13</a></li><li class="title summary">2013-05-30 - <a class="url" href="lecture.php?lid=158">CSS: Ch 14</a></li><li class="title summary">2013-06-10 WU: 1, 2</li><li class="title summary">2013-06-11 WU: 3, 4</li><li class="title summary">2013-06-12 WU: 5, 6</li><li class="title summary">2013-06-17 WU: 7, 8</li><li class="title summary">2013-06-18 WU: 9,10, 11</li></ol></section> <!-- ########### main Content ########### -->
+<footer>
+<p>Web designed by: <span id="author"></span>
+</p>
+</footer> <!-- end footer -->
+</div> <!-- id="wrapper" --> 
+</body>
+</html>
